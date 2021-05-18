f45ed0a66f71d7857182e42accc3815a15cdf1f0
Vili Vilenius <vili.vilenius@outlook.com>
2021-05-18 15:39:30 +0300

    Cleaned code and added some more UI

--- API.py
+++ API.py
@@ -6,236 +6,44 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 import time
 from numpy.polynomial import polynomial as poly
-# import numba
+import numba
 
 def lfit(*args):
     return poly.Polynomial.fit(*args)
-
+###═════════════════════════════════════════════════════════════════════
 class Data():
-    def __init__(self,x_data=None,y_data=None,n_data=None, b = 3):
-        if x_data:
-            self.x_data = x_data
-            self.n_data = len(self.x_data)
-            self.y_data = y_data if y_data else self.reference(self.x_data,b)
+    '''Data container'''
+    def __init__(self,x=None,y=None,n_data=None, b = 3):
+        if x:
+            self.x = x
+            self.n_data = len(self.x)
+            self.y = y if y else self.reference(self.x,b)
         else:
             self.n_data = int(1e5) if not n_data else int(n_data)
-            self.x_data = np.linspace(0,1,int(self.n_data))
-            self.y_data = self.reference(self.x_data,b)
+            self.x = np.linspace(0,1,int(self.n_data))
+            self.y = self.reference(self.x,b)
 
-        self.x_compressed = []
-        self.y_compressed = []
+        self.x_compressed = None
+        self.y_compressed = None
     #─────────────────────────────────────────────────────────────────── 
     def reference(self, x,c):
         # Setting up the reference data
         # return 2 - x/ (1+c-c*x)
         return np.sin(x**2*c*2)+3
     #───────────────────────────────────────────────────────────────────
-    def compress(self,atol=1e-5, mins=30,verbosity=0):
-        """Compresses the data
-        dy/dx < 0
-        d2y/dx2 < 0
-        """
-        t_start = time.perf_counter()
-
-        x_slice = self.x_data
-        y_slice = self.y_data
-        limit = self.n_data - 1
-        self.atol = atol
-        step = int(limit/5)
-        errscale = 0.5*np.max(np.abs((y_slice[-1]- y_slice[0])
-                                /(x_slice[-1] - x_slice[0])
-                                * (x_slice[::step]-x_slice[0])
-                                + y_slice[0] - y_slice[::step]
-                                )) / self.atol
-        scaler = errscale**0.5 + 1
-        estimate = int(limit/scaler)+1
-        # print("estimate",estimate)
-        #───────────────────────────────────────────────────────────────
-        def initial_f2zero(n):
-            n += 1
-            step = 1 if n<=mins*2 else int(n/((n*2 - mins)**0.5 + mins/2))
-            fit = lfit(x_slice[:n+1:step], y_slice[:n+1:step], 1)
-            err0 = abs(fit(x_slice[0])- y_slice[0])
-            errn = abs(fit(x_slice[n])- y_slice[n])
-            errmax = max(err0,errn) - self.atol
-            return errmax, fit
-        #───────────────────────────────────────────────────────────────
-        # print(initial_f2zero(0))
-        n2, fit = droot(initial_f2zero, -self.atol, estimate, limit)
-        # print("n2",n2)
-        n2 += 1
-        self.x_compressed.append(0)
-        self.y_compressed.append(fit(0))
-        x1 = x_slice[n2]
-        self.x_compressed.append(x1)
-        y1 = fit(x1)
-        self.y_compressed.append(y1)
-        #───────────────────────────────────────────────────────────────
-        def f2zero(n,xs, ys, x0, y0,atol):
-            step = 1 if n<=mins*2 else int(n/((n*2 - mins)**0.5 + mins/2))
-            Dx = xs[:n+1:step]-x0
-            Dy = ys[:n+1:step]-y0
-            a2 = np.sum(Dy*Dx)/np.sum(Dx*Dx)
-            a = a2
-            b = y0 - a * x0
-            fit = lambda x: a*x + b
-            err0 = abs(fit(xs[0])- ys[0])
-            errn = abs(fit(xs[n])- ys[n])
-            errmax = max(err0,errn) -atol
-            return errmax , fit
-        #───────────────────────────────────────────────────────────────
-        while n2 < limit:
-            x_slice = x_slice[n2+1:]
-            y_slice = y_slice[n2+1:]
-            limit -= n2 + 1
-            step = int(limit/10)
-            errscale = 0.5*np.max(np.abs((y_slice[-1]- y1)/(x_slice[-1] - x1)
-                                * (x_slice[1:limit:step]-x1)
-                                + y1 - y_slice[1:limit:step])) / self.atol
-            scaler = errscale**0.5 + 1
-            # print("from scaler",limit/scaler)
-            estimate = min(limit,int((limit/scaler+1+n2)/2))
-            # print("estimate",estimate)
-
-            n2, fit = droot(lambda n: f2zero(n, x_slice, y_slice, x1, y1,self.atol ),
-                            -self.atol, estimate, limit)
-            # print(limit)
-            # print(len(x_slice)-1)
-            # print("n2",n2)
-            # print(estimate/n2-1)
-            x1 = x_slice[n2]
-            self.x_compressed.append(x1)
-            y1 = fit(x1)
-            self.y_compressed.append(y1)
-        # print((x_slice[-1]))
-        # print(self.x_compressed[-1])
-
-        self.x_compressed = np.array(self.x_compressed)
-        self.y_compressed = np.array(self.y_compressed)
-        t = time.perf_counter()-t_start
-        if verbosity>0: print("Compression time\t%.1f ms" % (t*1e3))
-        if verbosity>0: print("Length of compressed array\t%i"%len(self.x_compressed))
-        compression_residual = len(self.x_compressed)/len(self.x_data)
-        if verbosity>0: print("Compression factor\t%.3f %%" % (compression_residual*1e2))
-    #───────────────────────────────────────────────────────────────
-    def simplecompress(self,atol=1e-5, mins=30,verbosity=0):
-
-        t_start = time.perf_counter()
-
-        x_slice = self.x_data
-        y_slice = self.y_data
-        limit = self.n_data - 3
-        self.atol = atol
-        #───────────────────────────────────────────────────────────────
-        def f2zero(n,xs, ys,atol):
-            n = int(n)+2
-            step = 1 if n<=mins*2 else int(n/((n*2 - mins)**0.5 + mins/2))
-            a = (ys[n] - ys[0])/(xs[n] - xs[0])
-            b = ys[0] - a * xs[0]
-            return max(np.abs(a*xs[0:n:step]+ b - ys[0:n:step]))-atol , None
-        #───────────────────────────────────────────────────────────────
-        self.x_compressed.append(x_slice[0])
-        self.y_compressed.append(y_slice[0])
-        step = int(limit/5)
-        errscale = 0.5*np.max(np.abs((y_slice[-1]- y_slice[0])
-                            /(x_slice[-1] - x_slice[0])
-                            * (x_slice[1:limit:step]-x_slice[0])
-                            + y_slice[0] - y_slice[1:limit:step])) / self.atol
-        scaler = errscale**0.5 + 1
-        estimate = min(limit,int(limit/scaler+1))
-        # print("estimate",estimate)
-
-        n2, _ = droot(lambda n: f2zero(n, x_slice, y_slice, self.atol),
-                        -self.atol, estimate, limit)
-        # print("n2",n2)
-        # print(estimate/n2-1)
-
-        while n2 < limit:
-            self.x_compressed.append(x_slice[n2+1])
-            self.y_compressed.append(y_slice[n2+1])
-            x_slice = x_slice[n2+1:]
-            y_slice = y_slice[n2+1:]
-
-            limit -= n2 + 2
-            step = int(limit/5)
-            errscale = 0.5*np.max(np.abs((y_slice[-1]- y_slice[0])
-                                /(x_slice[-1] - x_slice[0])
-                                * (x_slice[1:limit:step]-x_slice[0])
-                                + y_slice[0] - y_slice[1:limit:step])) / self.atol
-            scaler = errscale**0.5 + 1
-            # print("from scaler",limit/scaler)
-            estimate = min(limit,int((limit/scaler+1+n2)/2))
-            # print("estimate",estimate)
-
-            n2, _ = droot(lambda n: f2zero(n, x_slice, y_slice, self.atol),
-                            -self.atol, estimate, limit)
-            # print("n2",n2)
-            # print(estimate/n2-1)
-        #───────────────────────────────────────────────────────────────
-        self.x_compressed.append(x_slice[-1])
-        self.y_compressed.append(y_slice[-1])
-
-        self.x_compressed = np.array(self.x_compressed)
-        self.y_compressed = np.array(self.y_compressed)
-        t = time.perf_counter()-t_start
-        if verbosity>0: print("Compression time\t%.1f ms" % (t*1e3))
-        if verbosity>0: print("Length of compressed array\t%i"%len(self.x_compressed))
-        compression_residual = len(self.x_compressed)/len(self.x_data)
-        if verbosity>0: print("Compression factor\t%.3f %%" % (compression_residual*1e2))
-    #───────────────────────────────────────────────────────────────
-    def fastcompress(self, atol=1e-5, mins = 30, verbosity=0):
-        t_start = time.perf_counter()
-        # t_start = time.perf_counter()
-        self.atol = atol
-        def rec(a, b):
-            n = b-a-1
-            step = 1 if (n*2)<mins else int(round(n / ((n*2 - mins)**0.5 + mins/2)))
-
-            x1, y1 = self.x_data[a], self.y_data[a]
-            x2, y2 = self.x_data[b], self.y_data[b]
-            err = lambda x, y: np.abs((y2- y1) /(x2 - x1)* (x - x1) + y1 - y)
-            i = a + 1 + step*np.argmax(err(self.x_data[a+1:b-1:step], self.y_data[a+1:b-1:step]))
-            # print(index)
-            # time.sleep(0.5)
-            # print(a,b)
-            # print(err(self.x_data[index], self.y_data[index]))
-            return np.concatenate((rec(a, i), rec(i, b)[1:])) if err(self.x_data[i], self.y_data[i]) > atol else [a,b]
-        indices= rec(0,int(self.n_data-1))
-
-        self.x_compressed = self.x_data[indices]
-        self.y_compressed = self.y_data[indices]
-        t = time.perf_counter()-t_start
-        if verbosity>0: print("Compression time\t%.3f ms" % (t*1e3))
-        if verbosity>0: print("Length of compressed array\t%i"%len(self.x_compressed))
-        compression_residual = 1 - len(self.x_compressed)/len(self.x_data)
-        if verbosity>0: print("Compression factor\t%.3f %%" % (compression_residual*1e2))
-
-    #───────────────────────────────────────────────────────────────────
     def make_lerp(self):
-        self.lerp = interpolate.interp1d(self.x_compressed,
-                                         self.y_compressed,
-                                         assume_sorted=True)
+        self.lerp = interpolate.interp1d(self.x_compressed, self.y_compressed,
+                                            assume_sorted=True)
     #───────────────────────────────────────────────────────────────────
     def residuals(self):
-        """Error residuals"""
-        return self.lerp(self.x_data) - self.y_data
-
-
-# Given atol and Delta_y, 
-# in the best case 1 line would be enough 
-# and in the worst case Delta_y / atol.
-#
-# Geometric mean between these would maybe be good choice,
-# so likely around n_lines ~ sqrt(Delta_y / atol)
-# meaning delta_x ~ Delta_x * sqrt(atol / Delta_y)
-# 
-# When this is normalised so Delta_y = 1 and Delta_x = n,
-# delta_x ~ n * sqrt(atol)
-
+        '''Error residuals'''
+        return self.lerp(self.x) - self.y
+    #───────────────────────────────────────────────────────────────────
 ###═════════════════════════════════════════════════════════════════════
+### AUXILIARY FUNCTIONS
 def interval2(f,x1,y1,x2,y2):
-    """Returns the last x where f(x)<0
-    lerp"""
+    '''Returns the last x where f(x)<0
+    lerp'''
     
     while x2 - x1 > 2:
         # Average between linear estimate and half
@@ -260,36 +68,15 @@ def interval2(f,x1,y1,x2,y2):
         return (x1, fit)
 ###═════════════════════════════════════════════════════════════════════
 def droot(f, y0, x2, limit):
-    """Finds the upper limit to interval
+    '''Finds the upper limit to interval
     Limited 2nd degree polynomial
-    
-    Prediction heuristic 2
-        Function is approximately a*x^2 + y0
-        Fitting a*x1^2 + y0 = y1 -> a = (y1-y0)/x1^2
-        Root a*x^2 +y0 = 0 -> x = sqrt(-y0/a) 
-        Root a*xp^2 +y0 = 0 -> 
-        xp = sqrt(-y0/((y1-y0)/x1^2)) = x1 * sqrt(-y0/(y1-y0))
-        = x1 / sqrt(1-y1/y0) 
-        """
+    '''
     x1, y1 = 0, y0
     y2, fit = f(x2)
-    
+
     while y2 < 0:
-        # x2p = int(x2*(0 + 1/(1-y2/y0)**0.5))+2
-
-        # 0 = (y2-y1)/(x2-x1)*(x-x1) + y1
-        # x = -y1*(x2-x1)/(y2-y1) + x1
-        # Dx = -y1*(x2-x1)/(y2-y1)*2
-        
-        # x2l2 = int(-y2*(x2-x1)/(y2-y1)*2 + x2)
         x1, y1 = x2, y2
-        # x2l = int(x2 * (2/(1-y2/y0)**2-1))+2
-        # print("p",x2p)
-        # print("l",x2l)
-        # print("l2",x2l2)
-        # print("e",x2*2)
         x2 *= 2
-
         if x2 >= limit:
             y2, fit = f(limit)
             if y2<0:
@@ -299,6 +86,175 @@ def droot(f, y0, x2, limit):
                 break
         y2, fit = f(x2)
     return interval2(f,x1, y1, x2, y2)
+###═════════════════════════════════════════════════════════════════════
+# @numba.jit(nopython=True,cache=True)
+def n_lines(x,y,x0,y0,step,atol):
+    '''Estimates number of lines required to fit within error tolearance'''
+    errscale = 0.5*np.max(np.abs((y[-1]- y0)/(x[-1] - x0)* (x[:-1:step]-x0)+ y0
+     - y[:-1:step]))/atol
+    return errscale**0.5 + 1
+###═════════════════════════════════════════════════════════════════════
+### COMPRESSION FUNCTIONS
+def LSQ1(x,y,atol=1e-5, mins=10, verbosity=0, is_timed=False):
+    '''Compresses the data of type y = f(x) using linear least squares fitting
+    Works best if
+    dy/dx < 0
+    d2y/dx2 < 0
+    '''
+    if is_timed: t_start = time.perf_counter()
+    zero = 1
+    
+    left = len(x)-1
+    estimate = int(left/n_lines(x,y,x[0],y[0],int(left/5),atol) )+1
+    #───────────────────────────────────────────────────────────────
+    def initial_f2zero(n):
+        '''Function such that n is optimal when initialf2zero(n) = 0'''
+        step = 1 if n<=mins*2 else int(n / ((n * 2 - mins)**0.5 + mins / 2))
+        n += zero
+        fit = lfit(x[:n+1:step], y[:n+1:step], 1)
+        return max(abs(fit(x[0])- y[0]),abs(fit(x[n])- y[n])) - atol, fit
+    #───────────────────────────────────────────────────────────────
+    n2, fit = droot(initial_f2zero, -atol, estimate, left)
+    zero += n2 + 1
+    left -= zero
+    x_c = [0, x[zero-1]]
+    y_c = [fit(0), fit(x[zero-1])]
+    #───────────────────────────────────────────────────────────────
+    def f2zero(n):
+        '''Function such that n is optimal when f2zero(n) = 0'''
+        step = 1 if n<=mins*2 else int(n/((n*2 - mins)**0.5 + mins/2))
+        n += zero
+        Dx = x[zero:n+1:step]-x_c[-1]
+        Dy = y[zero:n+1:step]-y_c[-1]
+        a = np.sum(Dy*Dx)/np.sum(Dx*Dx)
+        b = y_c[-1] - a * x_c[-1]
+        fit = lambda x: a*x + b
+        return max(abs(fit(x[zero])-y[zero]),abs(fit(x[n])-y[n]))-atol, fit
+    #───────────────────────────────────────────────────────────────
+    while left > 0:
+    
+        estimate = int((n2 + left/(n_lines(x[zero+1:], y[zero+1:], 
+                                           x_c[-1], y_c[-1], 
+                                           int(left/10)+1, atol)))/2)
+        estimate = min(left, estimate)
+        n2, fit = droot(f2zero,-atol, estimate, left)
+        
+        zero += n2 + 1
+        left -= n2 + 1
+        x_c.append(x[zero-1])
+        y_c.append(fit(x_c[-1]))
+
+    if is_timed: t = time.perf_counter()-t_start
+    if verbosity>0: 
+        text = 'Length of compressed array\t%i'%len(x_c)
+        text += '\nCompression factor\t%.3f %%' % (100*len(x_c)/len(x))
+        if is_timed: text += '\nCompression time\t%.1f ms' % (t*1e3)
+        print(text)
+    
+    return np.array(x_c), np.array(y_c)
+###═════════════════════════════════════════════════════════════════════
+def pick(x,y,atol=1e-5, mins=30, verbosity=0, is_timed=False):
+
+    if is_timed: t_start = time.perf_counter()
+
+    x_slice = x
+    y_slice = y
+
+    limit = len(x)-3
+    atol = atol
+    #───────────────────────────────────────────────────────────────
+    def f2zero(n,xs, ys,atol):
+        n = int(n)+2
+        step = 1 if n<=mins*2 else int(n/((n*2 - mins)**0.5 + mins/2))
+        a = (ys[n] - ys[0])/(xs[n] - xs[0])
+        b = ys[0] - a * xs[0]
+        return max(np.abs(a*xs[0:n:step]+ b - ys[0:n:step]))-atol , None
+    #───────────────────────────────────────────────────────────────
+    x_c = [x_slice[0]]
+    y_c = [y_slice[0]]
+    step = int(limit/5)
+    scaler = n_lines(x_slice,y_slice,x_slice[0],y_slice[0],step,atol) 
+    estimate = min(limit,int(limit/scaler+1))
+    # print('estimate',estimate)
+
+    n2, _ = droot(lambda n: f2zero(n, x_slice, y_slice, atol),
+                    -atol, estimate, limit)
+    # print('n2',n2)
+    # print(estimate/n2-1)
+
+    while n2 < limit:
+        x_c.append(x_slice[n2+1])
+        y_c.append(y_slice[n2+1])
+        x_slice = x_slice[n2+1:]
+        y_slice = y_slice[n2+1:]
+
+        limit -= n2 + 2
+        step = int(limit/5)
+        errscale = 0.5*np.max(np.abs((y_slice[-1]- y_slice[0])
+                            /(x_slice[-1] - x_slice[0])
+                            * (x_slice[1:limit:step]-x_slice[0])
+                            + y_slice[0] - y_slice[1:limit:step])) / atol
+        scaler = errscale**0.5 + 1
+        # print('from scaler',limit/scaler)
+        estimate = min(limit,int((limit/scaler+1+n2)/2))
+        # print('estimate',estimate)
+
+        n2, _ = droot(lambda n: f2zero(n, x_slice, y_slice, atol),
+                        -atol, estimate, limit)
+        # print('n2',n2)
+        # print(estimate/n2-1)
+    #───────────────────────────────────────────────────────────────
+    x_c = np.array(x_c.append(x_slice[-1]))
+    y_c = np.array(y_c.append(y_slice[-1]))
+    if is_timed: t = time.perf_counter()-t_start
+    if verbosity>0: 
+        text = 'Length of compressed array\t%i'%len(x_c)
+        text += '\nCompression factor\t%.3f %%' % 100*len(x_c)/len(x)
+        if is_timed: text += '\nCompression time\t%.1f ms' % (t*1e3)
+        print(text)
+    return x_c, y_c
+###═════════════════════════════════════════════════════════════════════
+def split(x,y,atol=1e-5, mins=100, verbosity=0, is_timed=False):
+    t_start = time.perf_counter()
+    def rec(a, b):
+        n = b-a-1
+        step = 1 if (n*2)<mins else int(round(n / ((n*2 - mins)**0.5 + mins/2)))
+
+        x1, y1 = x[a], y[a]
+        x2, y2 = x[b], y[b]
+        err = lambda x, y: np.abs((y2- y1) /(x2 - x1)* (x - x1) + y1 - y)
+        i = a + 1 + step*np.argmax(err(x[a+1:b-1:step], y[a+1:b-1:step]))
+        # print(index)
+        # time.sleep(0.5)
+        # print(a,b)
+        # print(err(x[index], y[index]))
+        return np.concatenate((rec(a, i), rec(i, b)[1:])) if err(x[i], y[i]) > atol else [a,b]
+    indices= rec(0,len(x)-1)
+
+    x_c = x[indices]
+    y_c = y[indices]
+    if is_timed: t = time.perf_counter()-t_start
+    if verbosity>0:
+        text = 'Length of compressed array\t%i'%len(x_c)
+        text += '\nCompression factor\t%.3f %%' % 100*len(x_c)/len(x)
+        if is_timed: text += '\nCompression time\t%.1f ms' % (t*1e3)
+        print(text)
+    return x_c, y_c
+
+# Given atol and Delta_y, 
+# in the best case 1 line would be enough 
+# and in the worst case Delta_y / atol.
+#
+# Geometric mean between these would maybe be good choice,
+# so likely around n_lines ~ sqrt(Delta_y / atol)
+# meaning delta_x ~ Delta_x * sqrt(atol / Delta_y)
+# 
+# When this is normalised so Delta_y = 1 and Delta_x = n,
+# delta_x ~ n * sqrt(atol)
+methods = {'LSQ1': LSQ1,
+           'pick': pick,
+           'split': split}
+
 ###═════════════════════════════════════════════════════════════════════
 def fastcompress(x, y, atol=1e-5, mins = 100):
     '''Fast compression using sampling and splitting from largest error
@@ -317,4 +273,8 @@ def fastcompress(x, y, atol=1e-5, mins = 100):
 
         return np.concatenate((r(a,i), r(i,b)[1:])) if e(x[i], y[i]) > atol else (a,b)
 
-    return rec(0,len(x)-1)
+    return r(0,len(x)-1)
+
+def compress(*args, method='LSQ1', **kwargs):
+    '''Wrapper for easier selection of compression method'''
+    return methods[method](*args,**kwargs)
--- __main__.py
+++ __main__.py
@@ -13,107 +13,110 @@ import numpy as np
 
 helpstring = 'No arguments given'
 
-verbosity = 1 if ("--verbose" in sys.argv or '-v' in sys.argv) else 0
-is_plot = "--plot" in sys.argv
-is_save = "--save" in sys.argv
-is_show = "--show" in sys.argv
-print(verbosity)
+verbosity = 1 if ('--verbose' in sys.argv or '-v' in sys.argv) else 0
+is_timed = ('--timed' in sys.argv or '-t' in sys.argv)
+is_plot = '--plot' in sys.argv
+is_save = '--save' in sys.argv
+is_show = '--show' in sys.argv
 if len(sys.argv)==1:
     print(helpstring)
     exit()
 else:
     path_cwd = pathlib.Path(os.getcwd()).absolute()
-    if verbosity>0: print("Selected path is:\n\t%s" % path_cwd)
+    if verbosity>0: print('Selected path is:\n\t%s' % path_cwd)
 
     #───────────────────────────────────────────────────────────────────────
-    elif sys.argv[1] == "sandbox":
+    elif sys.argv[1] == 'sandbox':
         args = sys.argv[2:]
         import sandbox
     #───────────────────────────────────────────────────────────────────────
-    elif sys.argv[1] == "cwd":
+    elif sys.argv[1] == 'cwd':
         print(os.getcwd())
 
 
 path_home = pathlib.Path(__file__).parent.absolute()
 
-path_figures = path_home / "figures"
+path_figures = path_home / 'figures'
 ###═════════════════════════════════════════════════════════════════════
 n_data = int(float(sys.argv[1]))
 atol = float(sys.argv[2])
 mins = int(float(sys.argv[3]))
 b = int(float(sys.argv[4]))
 data = lc.Data(n_data=n_data,b=b)
-data.compress(atol=atol,mins = mins,verbosity=verbosity)
+data.x_compressed, data.y_compressed = lc.compress(data.x,data.y,
+                                                   atol=atol, mins = mins,
+                                                   verbosity = verbosity,
+                                                   is_timed = is_timed)
 #───────────────────────────────────────────────────────────────────
 if is_plot:
     plt.figure()
-    plt.plot(data.x_data,data.y_data)
-    plt.plot(data.x_compressed,data.y_compressed,"-o")
-    title = "LSQ compressed data"
+    plt.plot(data.x,data.y)
+    plt.plot(data.x_compressed,data.y_compressed,'-o')
+    title = 'LSQ compressed data'
     plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
+    if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
     #───────────────────────────────────────────────────────────────────
     data.make_lerp()
 
     plt.figure()
-    plt.plot(data.x_data,data.residuals())
-    title = "LSQ compressed residuals"
+    plt.plot(data.x,data.residuals())
+    title = 'LSQ compressed residuals'
     plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
+    if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
 #───────────────────────────────────────────────────────────────────
-data2 = lc.Data(n_data=n_data,b=b)
-data2.simplecompress(atol=atol,mins = mins,verbosity=verbosity)
+# data2 = lc.Data(n_data=n_data,b=b)
+# data2.simplecompress(atol=atol,mins = mins,verbosity=verbosity)
 
-if is_plot:
-    plt.figure()
-    plt.plot(data2.x_data,data2.y_data)
-    plt.plot(data2.x_compressed,data2.y_compressed,"-o")
-    title = "Simple compressed data"
-    plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
-    #───────────────────────────────────────────────────────────────────
-    data2.make_lerp()
-    plt.figure()
+# if is_plot:
+#     plt.figure()
+#     plt.plot(data2.x,data2.y)
+#     plt.plot(data2.x_compressed,data2.y_compressed,'-o')
+#     title = 'Simple compressed data'
+#     plt.title(title)
+#     if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
+#     #───────────────────────────────────────────────────────────────────
+#     data2.make_lerp()
+#     plt.figure()
     
-    plt.plot(data2.x_data,data2.residuals())
-    title = "Simple compressed residuals"
-    plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
-#───────────────────────────────────────────────────────────────────
-data3 = lc.Data(n_data=n_data,b=b)
-data3.fastcompress(atol=atol, mins = mins*10,verbosity = verbosity)
+#     plt.plot(data2.x,data2.residuals())
+#     title = 'Simple compressed residuals'
+#     plt.title(title)
+#     if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
+# #───────────────────────────────────────────────────────────────────
+# data3 = lc.Data(n_data=n_data,b=b)
+# data3.fastcompress(atol=atol, mins = mins*10,verbosity = verbosity)
 
-if is_plot:
-    plt.figure()
-    plt.plot(data3.x_data,data3.y_data)
-    plt.plot(data3.x_compressed,data3.y_compressed,"-o")
-    title = "Split compressed data"
-    plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
-    #───────────────────────────────────────────────────────────────────
-    data3.make_lerp()
+# if is_plot:
+#     plt.figure()
+#     plt.plot(data3.x,data3.y)
+#     plt.plot(data3.x_compressed,data3.y_compressed,'-o')
+#     title = 'Split compressed data'
+#     plt.title(title)
+#     if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
+#     #───────────────────────────────────────────────────────────────────
+#     data3.make_lerp()
 
-    plt.figure()
-    plt.plot(data3.x_data,data3.residuals())
-    title = "Split compressed residuals"
-    plt.title(title)
-    if is_save: plt.savefig(path_figures/(title+".png"), bbox_inches="tight")
+#     plt.figure()
+#     plt.plot(data3.x,data3.residuals())
+#     title = 'Split compressed residuals'
+#     plt.title(title)
+#     if is_save: plt.savefig(path_figures/(title+'.png'), bbox_inches='tight')
 
-# t_start = time.perf_counter()
-# indices = lc.fastcompress(data3.x_data, data3.y_data, atol=atol, mins = mins)
+# # t_start = time.perf_counter()
+# indices = lc.fastcompress(data3.x, data3.y, atol=atol, mins = mins)
 
-# data3.x_compressed, data3.y_compressed = data3.x_data[indices], data3.y_data[indices]
+# data3.x_compressed, data3.y_compressed = data3.x[indices], data3.y[indices]
 # t = time.perf_counter()-t_start
-# print("Compression time\t%.3f ms" % (t*1e3))
-# print("Length of compressed array\t%i"%len(xce))
-# compression_residual = 1 - len(xce)/len(data3.x_data)
-# print("Compression factor\t%.3f %%" % (compression_residual*1e2))
+# print('Compression time\t%.3f ms' % (t*1e3))
+# print('Length of compressed array\t%i'%len(xce))
+# compression_residual = 1 - len(xce)/len(data3.x)
+# print('Compression factor\t%.3f %%' % (compression_residual*1e2))
 
 # data3.make_lerp()
 # tol = abs(data3.residual())-data3.atol
 # print(max(tol))
 # plt.figure()
-# plt.plot(data3.x_data,tol)
+# plt.plot(data3.x,tol)
 
 # c = 0.5
 # n = 30
@@ -121,12 +124,12 @@ if is_plot:
 # xm = 2*x-1
 # y = xm*((1-c)*xm**2+c)/2+0.5
 # plt.figure()
-# plt.plot(x,y,"o")
-# plt.plot(y,np.zeros(n),"o")
+# plt.plot(x,y,'o')
+# plt.plot(y,np.zeros(n),'o')
 
 # density = 1/np.diff(y)
 # dx = x[:-1] + np.diff(x)/2
 # plt.figure()
-# plt.plot(dx,density/(min(density)),"o")
+# plt.plot(dx,density/(min(density)),'o')
 
 if is_show: plt.show()
